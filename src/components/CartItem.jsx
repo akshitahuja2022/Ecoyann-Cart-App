fe66f3fa6824d00627d7bcec491e79940e6d8ca0
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-05 19:28:52 +0530

    feat(ui): show the cart item details on the ui

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-
-const CartItem = () => {
-  return <div>CartItem</div>;
-};
-
-export default CartItem;
+export default function CartItem({ item }) {
+  return (
+    <div className="flex gap-4 border-2 border-gray-500 rounded-lg p-4 mb-4">
+      <img src={item.image} className="rounded-md" width={140} />
+      <div className="p-3">
+        <h2 className="font-bold text-xl">{item.product_name}</h2>
+        <p className="text-lg">₹{item.product_price}</p>
+        <p className="text-lg">Quantity: {item.quantity}</p>
+      </div>
+    </div>
+  );
+}
