b8624278a996c90bd668eda7fee36a593aabd4e6
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-05 19:30:42 +0530

    feat: create the ui for application and import the components

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { cartData } from "@/data/cartData";
 
-const page = () => {
-  return <div>Welcome to Ecommerce-Cart-App</div>;
-};
+import CartItem from "@/components/CartItem";
+import OrderSummary from "@/components/OrderSummary";
+import Link from "next/link";
 
-export default page;
+export default function CartPage() {
+  const subtotal = cartData.cartItems.reduce(
+    (acc, item) => acc + item.product_price * item.quantity,
+    0,
+  );
+
+  return (
+    <div className="p-10">
+      <h1 className="text-center text-2xl font-bold mb-10">
+        Welcome to Ecommerce-Cart-App
+      </h1>
+
+      <div className="flex gap-12 justify-center">
+        {cartData.cartItems.map((item) => (
+          <CartItem key={item.product_id} item={item} />
+        ))}
+      </div>
+
+      <div className="w-3/4 m-auto">
+        <OrderSummary subtotal={subtotal} shipping={cartData.shipping_fee} />
+      </div>
+
+      <div className="flex justify-center">
+        <Link href="/checkout">
+          <button className="bg-green-600 text-white text-lg font-semibold px-4 py-2 mt-8 rounded-md cursor-pointer">
+            Proceed to Checkout
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
