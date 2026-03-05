658b5165198c244a67bc861119388d75b15aacc1
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-05 19:47:52 +0530

    feat: create the context api for the application

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,3 +1,18 @@
-import { createContext } from "react";
+"use client";
 
-export const cartContext = createContext();
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export function CardProvider({ children }) {
+  const [address, setAddress] = useState(null);
+  const [form, setForm] = useState({
+    name: "",
+  });
+
+  return (
+    <CartContext.Provider value={{ form, setForm, address, setAddress }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
