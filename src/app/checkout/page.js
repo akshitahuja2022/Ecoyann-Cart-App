e117f2c1a1a7df8ecd3b26fc342a190a5a2f9957
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-05 19:47:34 +0530

    fix: remove the heading from checkout page

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -4,8 +4,7 @@ import AddressForm from "@/components/AddressForm";
 
 export default function CheckoutPage() {
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold mb-6">Shipping Address</h1>
+    <div>
       <AddressForm />
     </div>
   );
