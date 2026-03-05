482731eb5f28748031f3441f81541620205f1469
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-05 19:29:10 +0530

    feat(ui): show the summary of order

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+export default function OrderSummary({ subtotal, shipping }) {
+  const total = subtotal + shipping;
 
-const OrderSummary = () => {
-  return <div>Order Summary</div>;
-};
-
-export default OrderSummary;
+  return (
+    <div className="border-2 border-gray-500 rounded-lg p-4 mt-6">
+      <p className="text-lg">Subtotal: ₹{subtotal}</p>
+      <p className="text-lg">Shipping: ₹{shipping}</p>
+      <p className="font-bold text-lg mt-1">Total: ₹{total}</p>
+    </div>
+  );
+}
